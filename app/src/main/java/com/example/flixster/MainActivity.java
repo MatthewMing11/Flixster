4aed565ca13a8fc62409dc0fcab4e862d673172a
MatthewMing11 <Matthew.Ming11@myhunter.cuny.edu>
2021-02-25 18:05:25 -0500

    fixed Flixster

--- app/src/main/java/com/example/flixster/MainActivity.java
+++ app/src/main/java/com/example/flixster/MainActivity.java
@@ -52,8 +52,8 @@ public class MainActivity extends AppCompatActivity {
                 try{
                     JSONArray results = jsonObject.getJSONArray("results");
                     Log.i(TAG,"Results: " + results.toString());
-                    movies.addAll(Movie.fromJsonArray(results));
                     movieAdapter.notifyDataSetChanged();
+                    movies.addAll(Movie.fromJsonArray(results));
                     Log.i(TAG,"Movies: " + movies.size());
                 } catch(JSONException e){
                     Log.e(TAG, "Hit json exception",e);
